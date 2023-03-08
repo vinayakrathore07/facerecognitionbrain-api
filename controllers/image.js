37f41c8e6d11ff9a8f303a18f5c8f1872455b98c
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-08 10:47:55 +0530

    Add files via upload

---
+++ controllers/image.js
@@ -0,0 +1,47 @@
+const Clarifai = require('clarifai');
+
+
+	const app = new Clarifai.App({
+
+  apiKey: '60c7624765a94d0da89b167d2d1c9de6'
+
+});
+
+	const handleApiCall = (req,res ) => {
+
+	//   app.models
+	//   .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+	//   .then(data => {
+	//   	res.json(data);
+	//   })
+	//   .catch(err=> res.status(400).json('unable to work with API'))
+	// }
+	//   // above lines are old model
+	    app.models
+    .predict(
+      {
+        id: 'face-detection',
+        name: 'face-detection',
+        version: '6dc7e46bc9124c5c8824be4822abe105',
+        type: 'visual-detector',
+      }, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+ }
+
+const handleImage = (req, res,db ) => {
+const {id} = req.body;
+db('users').where('id','=', id )
+.increment('entries', 1)
+.returning('entries')
+.then(entries => {
+	res.json(entries[0].entries);
+})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+};
