30d382f11ea8f5d00f08c49b8d0c3c72d1ec5e72
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-11 19:18:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 
 
 const db = knex({
-  client: 'vinayak_07',
+  client: 'pg',
   connection: {
     hostname : 'dpg-cg5ba2bhp8u9l20imja0-a',
     port : '5432',
