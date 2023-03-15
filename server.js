2e2fd9cdfc9f8d8fc29439a278dae4dcf9fb14f4
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-15 09:38:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-aapp.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
