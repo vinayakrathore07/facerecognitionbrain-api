c93d687b4115edcae015dccb3b93d261ceb446fc
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-15 10:20:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res)=> {
-	res.send(database.users);
+	res.send('it is working!' )
 })
 
 app.post('/signin',(req, res)=> {signin.handleSignin(req, res, db, bcrypt)})
