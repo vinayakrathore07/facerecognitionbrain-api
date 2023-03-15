3e68765fccefa0af7ba254fcafa753641fa7959e
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-15 10:11:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,9 +29,7 @@ const db = knex({
 const app = express();
 
 app.use(cors())
-app.use(bodyParser.json());
-
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/', (req, res)=> {
 	res.send(database.users);
