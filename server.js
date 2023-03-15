dd7a56e1388dee367a17405dfbd006d220d4bf13
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-15 10:28:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,11 +31,12 @@ app.get('/', (req, res)=> {
 	res.send('it is working!' )
 })
 
-app.post('/signin',(req, res)=> {signin.handleSignin(req, res, db, bcrypt)})
-app.post('/register',(req, res) => {register.handleRegister(req, res, db ,bcrypt)})
-app.get ('/profile/:id', (req, res) => {profile.handleProfileGet(req,res,db)})
-app.put('/image', (req,res)=> {image.handleImage(req,res,db )})
-app.post('/imageurl', (req,res)=> {image.handleApiCall(req,res)})
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 app.listen(process.env.PORT || 3000,()=>{
 	console.log('app is running on port ${process.env.PORT}');
 })
