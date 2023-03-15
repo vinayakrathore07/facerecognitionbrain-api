3acba4a457587f47d9bfa7c3b1c469f30b8c95b3
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-15 09:45:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,12 +28,15 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res)=> {
+	res.send(db.users);
+})
+
+app.post('/signin',(req, res)=> {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register',(req, res) => {register.handleRegister(req, res, db ,bcrypt)})
+app.get ('/profile/:id', (req, res) => {profile.handleProfileGet(req,res,db)})
+app.put('/image', (req,res)=> {image.handleImage(req,res,db )})
+app.post('/imageurl', (req,res)=> {image.handleApiCall(req,res)})
 
 app.listen(process.env.PORT || 3000,()=>{
 	console.log('app is running on port ${process.env.PORT}');
