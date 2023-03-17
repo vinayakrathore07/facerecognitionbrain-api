5fcc50821ec309e11c300226deb4cd25bb708092
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-17 15:44:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,33 +11,25 @@ const image = require('./controllers/image');
 
 
 const db = knex({
-
   client: 'pg',
-
   connection: {
-
-    connectionString : process.env.DATABASE_URL,
-
+   connectionString : process.env.DATABASE_URL,
     ssl: true,
-
-  }});
+  }
+});
 
 const app = express();
 
 app.use(cors())
-app.use(express.json());
+app.use(bodyPraser.json());
 
-app.get('/', (req, res)=> {
-	res.send('it is working!' )
-})
-
-
+app.get('/', (req, res) => { res.send('success') })
 app.post('/signin',(req, res)=> {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register',(req, res) => {register.handleRegister(req, res, db ,bcrypt)})
 app.get ('/profile/:id', (req, res) => {profile.handleProfileGet(req,res,db)})
 app.put('/image', (req,res)=> {image.handleImage(req,res,db )})
 app.post('/imageurl', (req,res)=> {image.handleApiCall(req,res)})
-app.listen(process.env.PORT || 3000,()=>{
-	console.log('app is running on port ${process.env.PORT}');
+app.listen(process.env.PORT || 3001 ,()=> {
+	console.log('app is running on port 3001 ${process.env.PORT}');
 })
 
