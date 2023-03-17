381ddb8f52d36f7ad8872ec871f739beee5c3b46
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-17 18:19:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const image = require('./controllers/image');
 
 
 const db = knex({
-  client: 'pg',
-  connection: {
-   connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
+    client: 'pg',
+    connection: {
+        connectionString : process.env.DATABASE_URL,
+        ssl: true,
+    }
 });
 
 const app = express();
