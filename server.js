ba8bd7db5f8c54d9ff05d2b61e749612c72fe0ca
vinayakrathore07 <67449987+vinayakrathore07@users.noreply.github.com>
2023-03-20 10:34:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/register',(req, res) => {register.handleRegister(req, res, db ,bcrypt
 app.get ('/profile/:id', (req, res) => {profile.handleProfileGet(req,res,db)})
 app.put('/image', (req,res)=> {image.handleImage(req,res,db )})
 app.post('/imageurl', (req,res)=> {image.handleApiCall(req,res)})
-app.listen(process.env.PORT || 3001 ,()=> {
-	console.log('app is running on port 3001 ${process.env.PORT}');
+app.listen(process.env.PORT || 3000,()=>{
+console.log('app is running on port ${process.env.PORT}');
 })
 
